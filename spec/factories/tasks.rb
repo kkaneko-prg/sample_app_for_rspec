f2ada06d34a947b41ab431e2bfee37bccbd46927
金子夏純 <kanekokasumi@kkaneko.local>
2021-04-17 22:49:50 +0900

    02_ModelSpec

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :task do
-    
+    sequence(:title) { |n| "title#{n}" }
+    content { "RSpec編の課題2" }
+    status { :todo }
+    deadline { 1.week.from_now }
+    association :user
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    it 'すべての属性が有効であること' do
+      task = FactoryBot.build(:task)
+      task.valid?
+      expect(task.errors).to be_empty
+    end
+
+    it 'タイトルが未入力の場合に無効になること' do
+      without_task_title = FactoryBot.build(:task, title: nil)
+      without_task_title.invalid?
+      expect(without_task_title.errors[:title]).to include("can't be blank")
+    end
+
+    it 'ステータス未選択の場合に無効になること' do
+      without_task_status = FactoryBot.build(:task, status: nil)
+      without_task_status.invalid?
+      expect(without_task_status.errors[:status]).to include("can't be blank")
+    end
+
+    it 'タイトルが重複している場合に無効になること' do
+      task = FactoryBot.create(:task)
+      duplicate_task_title = FactoryBot.build(:task, title: task.title)
+      duplicate_task_title.invalid?
+      expect(duplicate_task_title.errors[:title]).to include("has already been taken")
+    end
+
+    it '別のタイトルだと有効になること' do
+      task = FactoryBot.create(:task)
+      another_task_title = FactoryBot.build(:task, title: 'test_title')
+      another_task_title.valid?
+      expect(another_task_title.errors).to be_empty
+    end
+  end
 end
