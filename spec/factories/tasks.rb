defbcf743bcbd0a8dc36d7a2eb57dec085a6b985
金子夏純 <kanekokasumi@kkaneko.local>
2021-04-17 23:28:31 +0900

    Fix: rails_helper ファイルにconfig.include FactoryBot::Syntax::Methodsを追記し、その他解答例とことなる記述を修正。

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :task do
-    sequence(:title) { |n| "title#{n}" }
-    content { "RSpec編の課題2" }
+    sequence(:title, "title_1")
+    content { "content" }
     status { :todo }
     deadline { 1.week.from_now }
     association :user
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,35 +3,35 @@ require 'rails_helper'
 RSpec.describe Task, type: :model do
   describe 'validation' do
     it 'すべての属性が有効であること' do
-      task = FactoryBot.build(:task)
-      task.valid?
+      task = build(:task)
+      expect(task).to be_valid
       expect(task.errors).to be_empty
     end
 
     it 'タイトルが未入力の場合に無効になること' do
-      without_task_title = FactoryBot.build(:task, title: nil)
-      without_task_title.invalid?
-      expect(without_task_title.errors[:title]).to include("can't be blank")
+      task_without_title = (:task, title: "")
+      expect(task_without_title).to be_invalid
+      expect(task_without_title.errors[:title]).to eq ["can't be blank"]
     end
 
     it 'ステータス未選択の場合に無効になること' do
-      without_task_status = FactoryBot.build(:task, status: nil)
-      without_task_status.invalid?
-      expect(without_task_status.errors[:status]).to include("can't be blank")
+      task_without_status = build(:task, status: nil)
+      texpect(task_without_status).to be_invalid
+      expect(task_without_status.errors[:status]).to eq ["can't be blank"]
     end
 
     it 'タイトルが重複している場合に無効になること' do
-      task = FactoryBot.create(:task)
-      duplicate_task_title = FactoryBot.build(:task, title: task.title)
-      duplicate_task_title.invalid?
-      expect(duplicate_task_title.errors[:title]).to include("has already been taken")
+      task = create(:task)
+      task_with_duplicated_title = build(:task, title: task.title)
+      expect(task_with_duplicated_title).to be_invalid
+      expect(task_with_duplicated_title.errors[:title]).to eq ["has already been taken"]
     end
 
     it '別のタイトルだと有効になること' do
-      task = FactoryBot.create(:task)
-      another_task_title = FactoryBot.build(:task, title: 'test_title')
-      another_task_title.valid?
-      expect(another_task_title.errors).to be_empty
+      task = create(:task)
+      task_with_another_title = build(:task, title: 'another_title')
+      expect(task_with_another_title).to be_valid
+      expect(task_with_another_title.errors).to be_empty
     end
   end
 end
